666cc62907c99f61f996894351afe9d66d351ad7
Johan Henningsson <johan129971@hotmail.com>
2026-01-06 08:56:30 +0100

    Initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TestSwedbank")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Diebold Nixdorf")]
+[assembly: AssemblyProduct("TP.net")]
+[assembly: AssemblyCopyright("Copyright (c) 2006 - 2017 Diebold Nixdorf")]
+[assembly: AssemblyTrademark("TP.net")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("46918b27-6b53-4899-bc1c-fc7eaef71871")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("60.7.9.25")]
+[assembly: AssemblyFileVersion("60.7.9.25")]
---
+++ ValidateJWT.cs
@@ -0,0 +1,140 @@
+﻿
+using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+namespace TPDotNet.MTR.Common
+{
+    [DataContract]
+    internal class JwtTimeClaims
+    {
+        // Claims can be numbers or strings; store as string and parse.
+        [DataMember(Name = "exp")]
+        public string Exp { get; set; }
+
+        //[DataMember(Name = "nbf")]
+        //public string Nbf { get; set; }
+
+        //[DataMember(Name = "iat")]
+        //public string Iat { get; set; }
+    }
+
+    
+    
+
+    public static class ValidateJWT
+    {
+        private static TPDotnet.Base.Service.TPBaseLogging objLog;
+        public static bool IsExpired(string jwt, TimeSpan? clockSkew = null, DateTime? nowUtc = null)
+        {
+            try
+            {
+                var now = nowUtc ?? DateTime.UtcNow;
+                var skew = clockSkew ?? TimeSpan.FromMinutes(5);
+
+                var exp = GetExpirationUtc(jwt);
+                if (exp == null) return false;
+                return now > exp.Value.Add(skew);
+            }
+            catch (Exception ex)
+            {
+                if (objLog == null)
+                {
+                    objLog = new TPDotnet.Base.Service.TPBaseLogging("ValidateJWT");
+                }
+                objLog.WriteLogError("ValidateJWT", "checkJWTExpired", ex);
+                return true;
+            }
+        }
+
+        public static bool IsValidNow(string jwt, TimeSpan? clockSkew = null, DateTime? nowUtc = null)
+        {
+            var now = nowUtc ?? DateTime.UtcNow;
+            var skew = clockSkew ?? TimeSpan.FromMinutes(5);
+
+            var claims = ParseClaims(jwt);
+            if (claims == null) return false;
+
+            //var nbf = ParseUnix(claims.Nbf);
+            var exp = ParseUnix(claims.Exp);
+
+            //if (nbf != null && now < nbf.Value.Subtract(skew)) return false;
+            if (exp != null && now > exp.Value.Add(skew)) return false;
+
+            return true;
+        }
+
+        public static DateTime? GetExpirationUtc(string jwt)
+        {
+            var claims = ParseClaims(jwt);
+            if (claims == null) return null;
+            return ParseUnix(claims.Exp);
+        }
+
+        //public static DateTime? GetIssuedAtUtc(string jwt)
+        //{
+        //    var claims = ParseClaims(jwt);
+        //    if (claims == null) return null;
+        //    return ParseUnix(claims.Iat);
+        //}
+
+        private static JwtTimeClaims ParseClaims(string jwt)
+        {
+            if (string.IsNullOrWhiteSpace(jwt)) return null;
+
+            var parts = jwt.Split('.');
+            if (parts.Length < 2) return null;
+
+            try
+            {
+                var payloadBytes = Base64UrlDecode(parts[1]);
+                using (var ms = new MemoryStream(payloadBytes))
+                {
+                    var ser = new DataContractJsonSerializer(typeof(JwtTimeClaims));
+                    var obj = ser.ReadObject(ms) as JwtTimeClaims;
+                    return obj;
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        private static DateTime? ParseUnix(string value)
+        {
+            if (string.IsNullOrEmpty(value)) return null;
+
+            long seconds;
+            if (!long.TryParse(value, out seconds)) return null;
+
+            try
+            {
+                var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+                return epoch.AddSeconds(seconds);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public static byte[] Base64UrlDecode(string input)
+        {
+            if (input == null) input = string.Empty;
+            string s = input.Replace('-', '+').Replace('_', '/');
+
+            switch (s.Length % 4)
+            {
+                case 2: s += "=="; break;
+                case 3: s += "="; break;
+                case 0: break;
+                default: throw new FormatException("Invalid Base64Url length.");
+            }
+
+            return Convert.FromBase64String(s);
+        }
+    }
+}
